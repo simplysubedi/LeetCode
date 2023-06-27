9e707720104d22cc8c2875fe00188635c7dbe639
Arjun Subedi <send2arjunsubedi@gmail.com>
2023-06-27 00:21:11 -0500

    ImplementPow(Updated)

--- ImplementPow.java
+++ ImplementPow.java
@@ -1,16 +1,31 @@
 public class ImplementPow {
-    public static void main(String[] args){
-        int x = 2;
-        int n = 10;
-        System.out.println("The result of pow ing is "+myPow(x,n));
+    public static double pow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n < 0) {
+            return 1 / powUtil(x, -((long) n));
+        } else {
+            return powUtil(x, n);
+        }
     }
 
-    private static int myPow(int x, int n) {
-        int result = 1;
-        while(n > 2){
-            result *= x;
-            n--;
+    private static double powUtil(double x, long n) {
+        if (n == 0) {
+            return 1;
         }
-        return result;
+
+        double half = powUtil(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+
+    public static void main(String[] args) {
+        double x = 2;
+        int n = -2147483648;
+        double result = pow(x, n);
+        System.out.println(x + " raised to the power of " + n + " is: " + result);
     }
 }
