c4f7aa3f7e5c7cf959a99d4e3a0b802d9c2a92e0
Arjun Subedi <send2arjunsubedi@gmail.com>
2023-12-06 22:40:36 -0600

    GoalParser

--- GoalParser.java
+++ GoalParser.java
@@ -3,22 +3,37 @@ public class GoalParser {
         String command = "G()()()()(al)";
         System.out.println("The parsed information is :"+interpret(command));
     }
+//    private static String interpret(String command) {
+//        String result = "";
+//        for (int i = 0; i < command.length(); i++){
+//            if(command.charAt(i) == 'G'){
+//                result +=command.charAt(i);
+//            }
+//            else if(command.charAt(i) == '(' && command.charAt(i + 1) == ')'){
+//                result += 'o';
+//                i++;
+//            }
+//            else if(command.charAt(i) == '(' && command.charAt(i +1) == 'a'){
+//                result +="al";
+//                i = i + 3;
+//            }
+//        }
+//        return  result;
+//    }
+private static String interpret(String command) {
+StringBuilder result = new StringBuilder();
 
-    private static String interpret(String command) {
-        String result = "";
-        for (int i = 0; i < command.length(); i++){
-            if(command.charAt(i) == 'G'){
-                result +=command.charAt(i);
-            }
-            else if(command.charAt(i) == '(' && command.charAt(i + 1) == ')'){
-                result += 'o';
-                i++;
-            }
-            else if(command.charAt(i) == '(' && command.charAt(i +1) == 'a'){
-                result +="al";
-                i = i + 3;
-            }
+        for (int i = 0; i < command.length(); i++) {
+        char currentChar = command.charAt(i);
+        if (currentChar == 'G') {
+            result.append("G");
+        } else if (currentChar == '(' && command.charAt(i + 1) == ')') {
+            result.append("o");
+            i++;
+        } else if (currentChar == '(' && command.charAt(i + 1) == 'a' && command.charAt(i + 2) == 'l' && command.charAt(i + 3) == ')') {
+            result.append("al");
+            i += 3;
         }
-        return  result;
     }
+        return result.toString();}
 }
