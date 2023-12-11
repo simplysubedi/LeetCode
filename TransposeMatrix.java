a238c5e365f65a19f93dfaf1986dad73306ffa2b
Arjun Subedi <send2arjunsubedi@gmail.com>
2023-12-11 10:03:00 -0600

    transpose

---
+++ TransposeMatrix.java
@@ -0,0 +1,42 @@
+public class TransposeMatrix {
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] originalMatrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(originalMatrix);
+
+        int[][] transposedMatrix = transpose(originalMatrix);
+
+        System.out.println("\nTransposed Matrix:");
+        printMatrix(transposedMatrix);
+    }
+
+    public static int[][] transpose(int[][] matrix) {
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int[][] transposeMatrix = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transposeMatrix[j][i] = matrix[i][j];
+            }
+        }
+
+        return transposeMatrix;}
+}
